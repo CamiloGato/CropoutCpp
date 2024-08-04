87cfb2be87ebf4b5e2bd0476e617b6d70596d6c9
CamiloGato <cm.andres022@hotmail.com>
2024-08-04 12:21:40 -0500

    Update: Creation of project

---
+++ Source/CropoutSampleProject/Public/Core/GMBase.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GMBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CROPOUTSAMPLEPROJECT_API AGMBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
