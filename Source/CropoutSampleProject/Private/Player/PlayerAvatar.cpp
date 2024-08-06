453408787734688751e593d9367c6acda1aa917e
CamiloGato <cm.andres022@hotmail.com>
2024-08-06 17:06:21 -0500

    Update: Updated with Cpp project

---
+++ Source/CropoutSampleProject/Private/Player/PlayerAvatar.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerAvatar.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values
+APlayerAvatar::APlayerAvatar()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	UCharacterMovementComponent* characterMovement = GetCharacterMovement();
+	characterMovement->bOrientRotationToMovement = true;
+}
+
+// Called when the game starts or when spawned
+void APlayerAvatar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerAvatar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APlayerAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
